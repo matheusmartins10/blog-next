c9aa7972a2c40b3a0cc5e08d31d420cb1dff94f0
Matheus Martins <mmartins.oliveira@outlook.com>
2020-09-24 11:43:51 -0300

    :memo: alteração no nome

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Matheus'
+const name = 'Matheus Martins'
 export const siteTitle   = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
